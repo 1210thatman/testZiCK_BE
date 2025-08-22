7be54c2f5ef08820e2d1e54eba85855cd5a16c0e
root <1210thatman@gmail.com>
2025-08-22 13:41:20 +0900

    first commit

---
+++ src/main/java/org/example/zick/ZiCkApplication.java
@@ -0,0 +1,13 @@
+package org.example.zick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiCkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiCkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zick/ZiCkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiCkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
